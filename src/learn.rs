ad588f1204b71efb076fd15e06ed140bc002d625
Junjun Dong <junjun.dong9@gmail.com>
2021-09-19 00:23:31 -0700

    fix write function

--- src/learn.rs
+++ src/learn.rs
@@ -1,9 +1,9 @@
+use anyhow::{anyhow, Result};
 use clap::{App, Arg};
 use env_logger;
 use hex::decode;
 use rustymind::{connect_headset, AsicEeg, PacketType, Parser, HEADSETID_AUTOCONNECT};
 use serde::{Deserialize, Serialize};
-use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 
@@ -27,16 +27,17 @@ impl Train {
         }
     }
 
-    fn write<W: Write>(&mut self, mut writer: W) -> serde_json::Result<()> {
-        serde_json::to_writer(&mut writer, &self)?;
-        writer.write_all(&"\n".as_bytes())?;
+    fn write<W: Write>(&mut self, mut writer: W) -> Result<()> {
+        let mut j = serde_json::to_string(&self)?;
+        j.push_str(&"\n");
+        writer.write_all(&j.as_bytes())?;
         *self = Train::new();
         Ok(())
     }
 }
 
 #[allow(unreachable_code)]
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     env_logger::init();
     let matches = App::new("rustymind")
         .version("1.0")
@@ -57,11 +58,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             decode(v).expect("Hex decoding failed")
         });
     let path = matches.value_of("dongle-path").unwrap();
-    let mut port = connect_headset(path, &headset[..])?;
+    let mut port = connect_headset(path, &headset[..]).map_err(|s| anyhow!(s))?;
     let mut read_buf: Vec<u8> = vec![0; 2048];
     let mut parser = Parser::new();
-    let eeg_power = AsicEeg::new();
-    let mut buffer = File::create("foo.txt")?;
+    let mut buffer = File::create("train_data.txt")?;
     let mut train_data = Train::new();
 
     loop {
@@ -70,29 +70,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         );
         for i in 0..bytes_read {
             if let Some(x) = parser.parse(read_buf[i]) {
-                let mut raw: Vec<i32> = Vec::new();
                 for r in x {
                     match r {
                         PacketType::RawValue(value) => {
-                            println!("Raw value = {}", value);
                             train_data.raw_val.push(value);
                         }
                         PacketType::PoorSignal(value) => {
-                            println!("Poor signal value = {}", value);
                             train_data.poor_signal = value;
                         }
                         PacketType::AsicEeg(value) => {
-                            println!("EEG power values = {:?}", value);
                             train_data.eeg = value;
                         }
                         PacketType::Attention(value) => {
-                            println!("Attention value = {}", value);
                             train_data.attention = value;
                         }
                         PacketType::Meditation(value) => {
-                            println!("Meditation value = {}", value);
                             train_data.meditation = value;
-                            train_data.write(buffer)?;
+                            train_data.write(&mut buffer)?;
                         }
                         PacketType::PacketUndefined(value) => {
                             println!("undefinded value = {}", value);
